27de943fa9331ac15869db9990dcd2b5eec68571
Naveen <naveen.amrita9@gmail.com>
2019-09-20 14:42:14 +0530

    News Application Using News API Services

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { NewsApiService } from './news-api.service';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule } from '@angular/material';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
+  ],
+  providers: [NewsApiService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/news-api.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsApiService {
+
+  api_key = '805fba8783874bd9b5909170ffe696b6';
+  constructor(private http:HttpClient) { }
+
+  initSources(){
+    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+this.api_key);
+ }
+ initArticles(){
+  return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key);
+ }
+ getArticlesByID(source: String){
+  return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+this.api_key);
+ }
+}
